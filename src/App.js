9713b171772fb9a653f44f17ea4d9ca34a8db8ca
Maisa Oliveira <maisaolisilva@gmail.com>
2024-04-02 15:31:00 -0300

    criando uma lista com as informações cadastrada pelos usuários

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 //Como o banner não possui um arquivo chamado "index.js" é necessário escrever o nome do arquivo a ser renderizado, fazendo com que fique Banner/Banner
+import { useState } from 'react';
 import Banner from './componentes/Banner/Banner';
 import Formulario from './componentes/Formulario';
 
 function App() {
+
+  const [animes, setAnimes] = useState([]);
+  const aoNovoAnimeAdicionado = anime => {
+    console.log(anime);
+    //set cria um array novo, pega a lista antiga e faz o expalhamento e acrescenta o novo anime
+    setAnimes([...animes, anime])
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario aoAnimeCadastrado={anime => aoNovoAnimeAdicionado(anime)}/>
     </div>
   );
 }
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -3,7 +3,8 @@ import './Formulario.css';
 import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
 import Botao from '../Botao';
-const Formulario = () => {
+import { useState } from 'react';
+const Formulario = (props) => {
 
     const generos = [
         'Shonen',
@@ -17,21 +18,73 @@ const Formulario = () => {
 
     ]
 
+    const [nome, setNome] = useState('');
+    const [diretor, setDiretor] = useState(''); 
+    const [estudio, setEstudio] = useState(''); 
+    const [observacoes, setObservacoes] = useState(''); 
+    const [imagem, setImagem] = useState(''); 
+    const [genero, setGenero] = useState('Shonen'); 
+
     const aoSalvar = (event) => {
-        event.preventDefaul();
-        console.log("Formulário fooi submetido");
+
+        event.preventDefault();
+        //mesma coisa de um objeto criado de maneira explicita:
+        //nome: nome,
+        //diretor: diretor
+        props.aoAnimeCadastrado({
+            nome,
+            diretor,
+            estudio,
+            observacoes,
+            imagem,
+            genero,
+        });
     }
 
     return (
         <section className='formulario'>
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do anime</h2>
-                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome"/>
-                <CampoTexto obrigatorio={true} label="Diretor" placeholder="Digite o nome do diretor"/>
-                <CampoTexto obrigatorio={true} label="Estúdio" placeholder="Digite o nome do estúdio"/>
-                <CampoTexto label="Observações" placeholder="Digite as observações em relação ao anime"/>
-                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
-                <ListaSuspensa obrigatorio={true} label='Gêneros' itens={generos} />
+                <CampoTexto 
+                    obrigatorio={true} 
+                    label="Nome" 
+                    placeholder="Digite seu nome" 
+                    valor={nome}
+                    aoAlterado={setNome}
+                />
+                <CampoTexto 
+                    obrigatorio={true} 
+                    label="Diretor" 
+                    placeholder="Digite o nome do diretor"
+                    valor={diretor}
+                    aoAlterado={setDiretor}
+                />
+                <CampoTexto 
+                    obrigatorio={true} 
+                    label="Estúdio" 
+                    placeholder="Digite o nome do estúdio"
+                    valor={estudio}
+                    aoAlterado={setEstudio}
+                    />
+                <CampoTexto 
+                    label="Observações" 
+                    placeholder="Digite as observações em relação ao anime"
+                    valor={observacoes}
+                    aoAlterado={setObservacoes}
+                />
+                <CampoTexto 
+                    label="Imagem" 
+                    placeholder="Digite o endereço da imagem"
+                    valor={imagem}
+                    aoAlterado={setImagem}    
+                />
+                <ListaSuspensa 
+                    obrigatorio={true} 
+                    label='Gêneros' 
+                    itens={generos} 
+                    valor={genero}
+                    aoAlterado={setGenero}
+                    />
                 <Botao>Criar Card</Botao>
             </form>
         </section>
